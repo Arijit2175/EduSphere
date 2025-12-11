bde742b5b2782ce8488d127c095aae3c5674fdc4
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-11 10:13:21 +0530

    router setup

--- elearn-frontend/src/App.jsx
+++ elearn-frontend/src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import FormalLearning from "./pages/FormalLearning";
+import NonFormalLearning from "./pages/NonFormalLearning";
+import InformalLearning from "./pages/InformalLearning";
+import AITutor from "./pages/AITutor";
+
 export default function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>E-Learning Platform</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/formal" element={<FormalLearning />} />
+        <Route path="/nonformal" element={<NonFormalLearning />} />
+        <Route path="/informal" element={<InformalLearning />} />
+
+        <Route path="/ai" element={<AITutor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
