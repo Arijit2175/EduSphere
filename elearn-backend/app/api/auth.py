6cc27342c2a10b099ca0ff9030f3d56444213b18
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-31 09:53:11 +0530

    password longer than 72 chars

--- elearn-backend/app/api/auth.py
+++ elearn-backend/app/api/auth.py
@@ -50,7 +50,7 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=2)):
     to_encode = data.copy()
