dd2adb3c13b46852ff4b041153727ee2678e0740
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-02-02 21:17:58 +0530

    fixing the arg error

--- elearn-frontend/src/components/AnimationPlayer.jsx
+++ elearn-frontend/src/components/AnimationPlayer.jsx
@@ -1,9 +1,17 @@
+import { useMemo } from "react";
 import Lottie from "lottie-react";
 import NotFoundAnim from "../assets/404.json";
 import SpinnerAnim from "../assets/Spinnerdots.json";
 
+const animationMap = {
+  "404": NotFoundAnim,
+  "spinner": SpinnerAnim,
+};
+
 export default function AnimationPlayer({ type = "404", loop = true, style }) {
-  const animationData = type === "spinner" ? SpinnerAnim : NotFoundAnim;
+  const animationData = useMemo(() => animationMap[type], [type]);
+
+  if (!animationData) return null;
 
   return (
     <div style={style}>
